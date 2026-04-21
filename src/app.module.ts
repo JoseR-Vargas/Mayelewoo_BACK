da88546a97bd9219ce588a8d97ba32a79c2779ae
Jose Vargas <rafael.uruguay2022@gmail.com>
2026-04-20 22:43:15 -0300

    codig muerto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module, OnModuleInit } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ContadoresModule } from './contadores/contadores.module';
 import { VouchersModule } from './vouchers/vouchers.module';
@@ -27,7 +26,6 @@ import { databaseConfig } from './config/database.config';
     CalculosMedidorModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements OnModuleInit {
   constructor(private authService: AuthService) {}
